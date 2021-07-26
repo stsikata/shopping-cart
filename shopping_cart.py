6e3d3bd90daf8ab2670ea600346e781853d32058
Sedina <sat9978l@nyu.edu>
2021-07-26 17:40:48 -0400

    Added csv readability

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,5 +1,11 @@
 # shopping_cart.py
 
+##from pandas import read_csv
+
+##products_df = read_csv("data/products.csv")
+
+##products = products_df.to_dict("records")
+
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
@@ -46,7 +52,6 @@ def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
 
-# TODO: write some Python code here to produce the desired output
 selected_products = []
 
 while True:
@@ -68,15 +73,7 @@ while True:
         else:
             print("Whoops, please make sure you've entered the correct product identifier.")         
             
-            
-# selected_product = selected_products[0]
-#####print(selected_product)
-            ###else: ____
-            #if str(item["id"]) != str(selected_id):
-             #   print("Whoops, please make sure you've entered the correct product identifier.")
-  # if # correct, print(products["name"], products["price"])
-#print(products["name"], products["price"])
-#print(selected_products)
+
 print("You selected", len(selected_products), "items.", "Below is your receipt.")
 
 print(" ---------------------------------")
@@ -85,16 +82,14 @@ print("(555)-555-5555")# A grocery store phone number and/or website URL and/or 
 print(" ---------------------------------")
 import datetime
 now = datetime.datetime.now()
-#print(type(now))
+# print(type(now))
 #print("Checkout At:", now) #The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
 print("Checked Out At:", now.strftime("%Y-%m-%d %H:%M:%S"))
 print(" ---------------------------------")
-# # The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
+# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
 from operator import itemgetter
 sorted_selected_products= sorted(selected_products, key=itemgetter("name"))
 print("********************")
-# print(sorted_selected_products)
-# print(type(sorted_selected_products))
 
 
 for item in sorted_selected_products:
@@ -104,25 +99,23 @@ for item in sorted_selected_products:
 
 from statistics import mean
 print(" ---------------------------------")
-# # The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
+# The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
 #for item in sorted_selected_products:
-    #print(item.keys())
-    #print(to_usd(item["price"]))
+   #print(item.keys())
+   #print(to_usd(item["price"]))
 prices = [item["price"] for item in sorted_selected_products]
 subtotal = sum(prices)
 
 print("Subtotal:", to_usd(subtotal))
 
-# # The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
-# tax = float(subtotal) * 0.0875
-#tax = subtotal * 0.0875 ## I guess I don't need to convert it to a float now that I've removed the dollar sign?
+# The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
 tax = subtotal * float(z)
 
 print("Sales tax:", to_usd(tax))
 print(" ---------------------------------")
 print(" ---------------------------------")
 
-# # The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+# The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
 total_price = subtotal + tax
 print("**TOTAL:**", to_usd(total_price))
 
@@ -135,19 +128,7 @@ else:
     print("Thank you, please come back soon :)")# A friendly message thanking the customer and/or encouraging the customer to shop again
 
 
-print("********")
-
-
-print(type(selected_products))
-print(selected_products)
-
-print(type(sorted_selected_products))
-print(sorted_selected_products)
-
-print("********")
-#print(products)
-
-######## EMAIL
+####### EMAIL
 #import os
 #from dotenv import load_dotenv
 from sendgrid import SendGridAPIClient
@@ -166,28 +147,6 @@ template_data = {
         sorted_selected_products
 }
 
-# this must match the test data structure
-###
-# receipt_data = {
-#      "total_price_usd": "$1.00", #to_usd(total_price),
-#      "human_friendly_timestamp": "July 4th, 2099 10:00 AM", #now.strftime("%Y-%m-%d %H:%M:%S"),
-#      "products": selected_products
-#      ]
-# }
-##
-
-# receipt_data = {
-#     "total_price_usd": to_usd(total_price),
-#     "human_friendly_timestamp": now.strftime("%Y-%m-%d %H:%M:%S"),
-#     "sort_selected": sorted_selected_products[0]
-    # "products":[
-    #     {"id":1, "name": "Product 1"},
-    #     {"id":2, "name": "Product 2"},
-    #     {"id":3, "name": "Product 3"},
-    #     {"id":2, "name": "Product 2"},
-    #     {"id":1, "name": "Product 1"}
-   # ]
-#} # or construct this dictionary dynamically based on the results of some other process :-D
 
 client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
 print("CLIENT:", type(client))
